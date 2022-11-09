59d550182f20339ee956ad3bc1629c11259dbf8e
rhdhltnr123 <113665798+rhdhltnr123@users.noreply.github.com>
2022-11-09 13:36:39 +0900

    수정
    수정

---
+++ js/main2.js
@@ -0,0 +1,40 @@
+// main.js
+console.log("main.js");
+
+$(function () {
+  // 배너 이미지 배열
+  let bgArr = [
+    "images/Introduce1.jpg",
+    "images/Introduce2.jpg",
+    "images/Introduce3.jpg",
+  ];
+
+  let bg_img_num = 0; // 이미지 번호
+
+  // 배경 재생
+  setInterval(function () {
+    bg_img_num++; // 이미지 번호 증가
+    if (bg_img_num >= bgArr.length) {
+      bg_img_num = 0;
+    }
+    console.log(bg_img_num);
+    // 배경이미지 변경
+    $("#main-banner .container").css({
+      "background-image": `url(${bgArr[bg_img_num]})`,
+    });
+  }, 5000);
+});
+
+// 메인 이미지 슬라이더
+$(function () {});
+
+// 사이트맵 버튼
+$(function () {
+  $("#btn-toggle").on("click", function () {
+    $("#site-map-menu").toggleClass("show");
+  });
+
+  $("#site-menu-close").on("click", function () {
+    $("#site-map-menu").removeClass("show");
+  });
+});
